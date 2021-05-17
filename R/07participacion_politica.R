81d0f69728e3407dbdddb044ec15531bc5afb50f
mmgerber <66801438+mmgerber@users.noreply.github.com>
2021-05-17 15:11:04 -0400

    Update 07participacion_politica.R

--- R/07participacion_politica.R
+++ R/07participacion_politica.R
@@ -3,57 +3,57 @@
 pacman::p_load(tidyverse, summarytools, sjPlot)
 
 # 2. Load data  -------------------------------------------
-data <- readRDS(file = "output/data/movid_i.RDS")
+data <- readRDS(file = "output/data/movid_o.RDS")
 
 # 3. Recoding
 data <- data %>% 
   mutate(disp_protesta = case_when(
-    f5_1 == "De acuerdo" ~ 1,
-    f5_1 == "En desacuerdo" ~ 0,
-    f5_1 == "Indiferente" ~ 0,
-    f5_1 == "Muy de acuerdo" ~ 1,
-    f5_1 == "Muy en desacuerdo" ~ 0),
+    f5_1 == 4 ~ 1,
+    f5_1 == 2 ~ 0,
+    f5_1 == 3 ~ 0,
+    f5_1 == 5 ~ 1,
+    f5_1 == 1 ~ 0),
     protesta = case_when(
-      f4_1 == "Sí" ~ 1,
-      f4_1 == "No" ~ 0),
+      f4_1 == 1 ~ 1,
+      f4_1 == 2 ~ 0),
     comunitaria_dar = case_when(
-      f4_2 == "Sí" ~ 1,
-      f4_2 == "No" ~ 0),
+      f4_2 == 1 ~ 1,
+      f4_2 == 2 ~ 0),
     comunitaria_recibir = case_when(
-      f4_3 == "Sí" ~ 1,
-      f4_3 == "No" ~ 0),
+      f4_3 == 1 ~ 1,
+      f4_3 == 2 ~ 0),
     riesgo = case_when(
-      f6 == "Nada peligroso" ~ 1,
-      f6 == "Algo peligroso" ~ 2,
-      f6 == "Bastante peligroso" ~ 3,
-      f6 == "Muy peligroso" ~ 5,
-      f6 == "Extremadamente peligroso" ~ 1),
+      f6 == 1 ~ 1,
+      f6 == 2 ~ 2,
+      f6 == 3 ~ 3,
+      f6 == 4 ~ 4,
+      f6 == 5 ~ 5),
     cae = case_when(
-      g1 == "Sí" ~ "Ocupado",
-      g1 == "No" & g5 == "Sí" ~ "Desocupado",
-      g1 == "No" & g5 == "No" ~ "Inactivo"),
+      g1 == 1 ~ "Ocupado",
+      g1 == 2 & g5 == 1 ~ "Desocupado",
+      g1 == 2 & g5 == 2 ~ "Inactivo"),
     cambio_ingreso = case_when(
-      g49 == "Ha aumentado. ¿En qué porcentaje?" ~ "Aumentado",
-      g49 == "Ha bajado. ¿En qué porcentaje?" ~ "Bajado",
-      g49 == "Se han mantenido constante" ~ "Igual"),
+      g49 == 3 ~ "Aumentado",
+      g49 == 1 ~ "Bajado",
+      g49 == 2 ~ "Igual"),
     desigualdad = case_when(
-      f3_1 == "De acuerdo" ~ 4,
-      f3_1 == "En desacuerdo" ~ 2,
-      f3_1 == "Indiferente" ~ 3,
-      f3_1 == "Muy de acuerdo" ~ 5,
-      f3_1 == "Muy en desacuerdo" ~ 1),
+      f3_1 == 4 ~ 4,
+      f3_1 == 2 ~ 2,
+      f3_1 == 3 ~ 3,
+      f3_1 == 5 ~ 5,
+      f3_1 == 1 ~ 1),
     gobierno_bienestar = case_when(
-      f3_2 == "De acuerdo" ~ 4,
-      f3_2 == "En desacuerdo" ~ 2,
-      f3_2 == "Indiferente" ~ 3,
-      f3_2 == "Muy de acuerdo" ~ 5,
-      f3_2 == "Muy en desacuerdo" ~ 1),
+      f3_2 == 4 ~ 4,
+      f3_2 == 2 ~ 2,
+      f3_2 == 3 ~ 3,
+      f3_2 == 5 ~ 5,
+      f3_2 == 1 ~ 1),
     obediencia = case_when(
-      f3_3 == "De acuerdo" ~ 4,
-      f3_3 == "En desacuerdo" ~ 2,
-      f3_3 == "Indiferente" ~ 3,
-      f3_3 == "Muy de acuerdo" ~ 5,
-      f3_3 == "Muy en desacuerdo" ~ 1),
+      f3_3 == 4 ~ 4,
+      f3_3 == 2 ~ 2,
+      f3_3 == 3 ~ 3,
+      f3_3 == 5 ~ 5,
+      f3_3 == 1 ~ 1),
     )
 
 
