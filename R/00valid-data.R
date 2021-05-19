52f2a93ad84f8898ff84e75587306c19b16ee21e
valentinaandrade <valentinaandrade@uchile.cl>
2021-05-19 11:22:25 -0400

    compare data frames

--- R/00valid-data.R
+++ R/00valid-data.R
@@ -4,12 +4,27 @@ pacman::p_load(tidyverse, arsenal)
 
 # 2. Load data  -------------------------------------------
 ## movid_i-19
-movid_o <- haven::read_dta("input/data/210123_base_movid_version02.dta")
-
-x <- haven::read_dta("input/data/20210125_base_movid_version_codificada.dta")
+movid_o_antigua <- haven::read_dta("input/data/210123_base_movid_version02.dta")
+movid_o_csv <- read.csv("input/data/20210125_base_movid_version_codificada.csv",
+                        na.strings=c("","NA", ""),
+                        header = TRUE, fileEncoding = "UTF-8")
+movid_o <- haven::read_dta("input/data/20210125_base_movid_version_codificada.dta")
 
 # Compare
-z <- anti_join(x, movid_i)
+movid_o_cambios <- anti_join(movid_o, movid_o_antigua)
 #59 obs distintas
-z <- summary(comparedf(x,movid_o))
-z$diffs.table
+movid_o_cambios <- summary(comparedf(movid_o,movid_o_antigua))
+movid_o_cambios$diffs.table
+
+
+# Likerts -----------------------------------------------------------------
+movid <- movid_o %>% select(id_encuesta, starts_with("f3"),starts_with("f5"), starts_with("f7")) %>% 
+  mutate_at(vars(starts_with("f")), funs(as_factor(.))) %>% 
+  mutate_at(vars(starts_with("f")), funs(as.character(.))) 
+
+movid_csv <- movid_o_csv %>% select(id_encuesta,starts_with("f3"),starts_with("f5"), starts_with("f7")) %>% 
+  mutate_at(vars(starts_with("f")), funs(as_factor(.))) %>% 
+  mutate_at(vars(starts_with("f")), funs(as.character(.)))
+  
+movid_o_cambios <- summary(comparedf(movid,movid_csv))
+movid_o_cambios$diffs.table
